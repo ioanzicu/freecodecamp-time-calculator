69ed01d7580f864a4528595f9ab2ccce9d6407b2
Ioan <ioan.zicu94@gmail.com>
2021-05-05 01:50:17 +0200

    extract logic into separate functions

--- time_calculator.py
+++ time_calculator.py
@@ -2,42 +2,54 @@ from datetime import datetime, timedelta
 import calendar
 
 
-def add_time(start, duration, week_day=''):
+def get_weekday(week_day, days_diff) -> str:
+    '''Returns weekday index.'''
+    days_of_week = list(calendar.day_name)
+    start_day_index = 0
+    for index, day in enumerate(days_of_week):
+        if week_day.capitalize() == day:
+            start_day_index = index
+            break
 
+    final_weekday_index = (
+        start_day_index + days_diff) % 7  # days of week
+    weekday = days_of_week[final_weekday_index]
+
+    return weekday
+
+
+def get_duration_delta(duration) -> timedelta:
+    '''Returns a timedelta object with specified time in the duration parameter.'''
+    try:
+        duration_hour, duration_minutes = duration.split(':')
+        duration_hour = int(duration_hour)
+        duration_minutes = int(duration_minutes)
+
+        return timedelta(
+            hours=duration_hour, minutes=duration_minutes)
+    except Exception as err:
+        print(err)
+        exit()
+
+
+def add_time(start, duration, week_day='') -> str:
     try:
         start_format_str = '%I:%M %p'  # %I - 12 hours format;  %H - 24 hours format
         start_time = datetime.strptime(start, start_format_str)
 
-        duration_hour, duration_minutes = duration.split(':')
-
-        duration_hour = int(duration_hour)
-        duration_minutes = int(duration_minutes)
-
-        duration_delta = timedelta(
-            hours=duration_hour, minutes=duration_minutes)
-
+        duration_delta = get_duration_delta(duration)
         final_time = start_time + duration_delta
 
-        # %#I (Windows) | %-I (Unix) - will remove 0 when hour is < 10
+        # %#I (Windows) | %-I (Unix) - remove 0 when hour is < 10
         final_format_str = '%#I:%M %p'
         final_time_str = final_time.strftime(final_format_str)
 
         days_diff = final_time.day - start_time.day
 
-        days_of_week = list(calendar.day_name)
         if week_day:
-            start_day_index = 0
-            for index, day in enumerate(days_of_week):
-                if week_day.capitalize() == day:
-                    start_day_index = index
-                    break
+            final_weekday = get_weekday(week_day, days_diff)
+            final_time_str += f', {final_weekday}'
 
-            final_weekday_index = (
-                start_day_index + days_diff) % 7  # days of week
-            final_time_weekday = days_of_week[final_weekday_index]
-            final_time_str += f', {final_time_weekday}'
-
-        # print(final_time.weekday())
         if days_diff == 1:
             final_time_str += ' (next day)'
         elif days_diff > 1:
@@ -48,18 +60,3 @@ def add_time(start, duration, week_day=''):
     except Exception as err:
         print(err)
         exit()
-
-
-if __name__ == '__main__':
-    d = add_time("3:00 PM", "3:10")
-    print(d)
-    d = add_time("11:30 AM", "2:32", "Monday")
-    print(d)
-    d = add_time("11:43 AM", "00:20")
-    print(d)
-    d = add_time("10:10 PM", "3:30")
-    print(d)
-    d = add_time("11:43 PM", "24:20", "tueSday")
-    print(d)
-    d = add_time("6:30 PM", "205:12")
-    print(d)
